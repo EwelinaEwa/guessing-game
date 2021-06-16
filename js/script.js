ad3c3e46527fc30a8efa97dce74589cbe0b2fb3c
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-16 11:53:49 +0200

    Output result of the game to console

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@
     const secretNumber = getSecretNumber(students);
     console.log(secretNumber)
 
+    document.getElementById("guessButton").addEventListener("click", function () {
+        const guessedNumber = Number(document.getElementById("guessNumber").value);
+        console.log(guessedNumber)
+
+        if (secretNumber == guessedNumber) {
+            console.log(`Awesome! You number ${guessedNumber} was correct. You can be named many things, hungry not being one of them.`)
+        }
+        else {
+            console.log(`Bummer... You guessed ${guessedNumber} and the secret number was ${secretNumber}.`)
+        }
+    })
+
 
 })();
