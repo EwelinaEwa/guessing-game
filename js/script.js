5d0c49545fc233601d4507cba78f9557a9215c6d
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-06-16 12:06:27 +0200

    Add feature, where if only wrong by +/-1 different outpou is given

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@
 
     let students = 15;
     const secretNumber = getSecretNumber(students);
-    console.log(secretNumber)
 
     document.getElementById("guessButton").addEventListener("click", function () {
         const guessedNumber = Number(document.getElementById("guessNumber").value);
-        console.log(guessedNumber)
 
         if (secretNumber == guessedNumber) {
             document.getElementById("result").innerHTML = `Awesome! Your number ${guessedNumber} was correct. You can be named many things, hungry not being one of them.`
+        }
+        else if (secretNumber == guessedNumber+1 || secretNumber == guessedNumber-1) {
+            document.getElementById("result").innerHTML = `So close, but you just missed it! Are you in a class that started on the thirteenth or what?`
         }
         else {
             document.getElementById("result").innerHTML = `Bummer... You guessed ${guessedNumber} and the secret number was ${secretNumber}.`
